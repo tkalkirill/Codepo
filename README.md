f5717ef9e2e6c43aa37bb59f093803fecabddd9a
Kirill Tkalenko <tkalkirill@yandex.ru>
2025-09-30 11:50:46 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Codepo.
+
+This is a simple and home-based project to prepare for technical interviews.
---
+++ src/main/java/org/codepo/Main.java
@@ -0,0 +1,7 @@
+package org.codepo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
